6c55a999237982f2ece3347757fb2c5ddacf406d
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-05-09 08:45:09 -0400

    updated database.js to get all from jateDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,6 @@
 import {openDB} from "idb";
 
+// JATE: Just Another Text Editor
 const initdb = async () =>
   openDB("jate", 1, {
     upgrade(db) {
@@ -15,9 +16,9 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log("Post to the Dbase");
-  const codeDb = await openDB("code", 1);
-  const tx = codeDb.transaction("code", "readwrite");
-  const store = tx.objectStore("code");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
   const request = store.add({
     data: content,
   });
@@ -26,6 +27,15 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error("getDb not implemented");
+export const getDb = async () => {
+  console.log("GET all from the database");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+};
 
 initdb();
