303faad780a0e4e92e0bfb603f839f866282e714
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-05-09 20:34:12 -0400

    working editor.js

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -24,20 +24,32 @@ console.log(`local storage: ${localData}`)
 
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
-    getDb().then((data) => {
+    getDb().then((res) => {
+      const index = res.length;
+      console.log(index);
+      const localIndex = localData.length;
+      // console.log(res[res.length-1].data);
+      // console.log(localData);
+      // console.log(header);
       // console.log(res);
       // const index = res.length-1;
       // console.log(index);
       // console.info("Loaded data from THIS, injecting into editor");
-      if (data.length > 0) {
+      // console.log(data.length)
+      if (index != 0) {
+        console.log('data.length is >0')
         // console.log(res[0].data)
-        this.editor.setValue(data);
-      } else if (localData.length > 0) {
+        this.editor.setValue(res[res.length-1].data);
+      } 
+      else if (localIndex != 0) {
+        console.log('localData.length is >0')
         this.editor.setValue(localData);
-      } else {
+      }
+       else {
+        console.log('header section')
         this.editor.setValue(header);
       }
-      // this.editor.setValue(res || localData || header);
+    // this.editor.setValue(res[res.length-1].data || localData || header);
     });
 
     this.editor.on("change", () => {
